13bb3ce1cd92bf67533763b8d9ca8ecc7e14394a
stropitek <kostro.d@gmail.com>
2016-02-02 13:14:13 +0100

    fix getLastSeqIds

--- src/mongo/models/seqIdAggregated.js
+++ src/mongo/models/seqIdAggregated.js
@@ -4,12 +4,14 @@ const model = require('./../model');
 const Model = model.getSeqIdAggregated();
 
 exports.getLastSeqIds = function (name) {
-    return Model.findById(name).exec();
+    return Model.findById(name).exec().then(result => {
+        if(result) return result.seq;
+        return null;
+    });
 };
 
 exports.setSeqIds = function(name, seqIds) {
-
-    return Model.findByIdAndUpdate(name, seqIds, {new: true, upsert: true})
+    return Model.findByIdAndUpdate(name, {seq: seqIds}, {new: true, upsert: true})
         .exec()
         .then(result => result.seq);
 };
