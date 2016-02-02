c864ddd0369cc1200e3c6914d1fcad762c482083
stropitek <kostro.d@gmail.com>
2016-02-02 17:20:52 +0100

    add cors

--- src/api/server.js
+++ src/api/server.js
@@ -3,9 +3,10 @@
 const koa = require('koa');
 const router = require('./router');
 const bodyParser = require('koa-body');
+const kcors = require('kcors');
 
 const app = koa();
-
+app.use(kcors());
 app.use(bodyParser({
     jsonLimit: '100mb'
 }));
