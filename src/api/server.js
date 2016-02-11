4541c76f95702f2be8e9686c4d4f06598381b368
Michaël Zasso <mic.besace@gmail.com>
2016-02-11 16:02:31 +0100

    allow to add custom middlewares to the API

--- src/api/server.js
+++ src/api/server.js
@@ -4,12 +4,18 @@ const koa = require('koa');
 const router = require('./router');
 const bodyParser = require('koa-body');
 const kcors = require('kcors');
+const config = require('../config/config');
 
 const app = koa();
 app.use(kcors());
 app.use(bodyParser({
     jsonLimit: '100mb'
 }));
+
+if (config.server && config.server.middleware) {
+    config.server.middleware.forEach(middleware => app.use(middleware));
+}
+
 app.use(router.routes());
 
 module.exports = app;
