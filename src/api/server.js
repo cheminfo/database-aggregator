b19bf4583ad987477fbf72a66d8de8de6e0efd23
Michaël Zasso <mic.besace@gmail.com>
2016-02-25 10:53:13 +0100

    add koa-compress and koa-response-time

--- src/api/server.js
+++ src/api/server.js
@@ -3,10 +3,14 @@
 const koa = require('koa');
 const router = require('./router');
 const bodyParser = require('koa-body');
+const compress = require('koa-compress');
+const responseTime = require('koa-response-time');
 const kcors = require('kcors');
 const config = require('../config/config').globalConfig;
 
 const app = koa();
+app.use(compress());
+app.use(responseTime());
 app.use(kcors());
 app.use(bodyParser({
     jsonLimit: '100mb'
