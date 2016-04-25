8331b2cdaeab2376ab43577fd82c7b05693b5ccf
stropitek <kostro.d@gmail.com>
2016-04-25 14:55:22 +0200

    fix aggregate and source workers

--- src/aggregation/worker.js
+++ src/aggregation/worker.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const aggregate = require('./aggregate');
+const Promise = require('bluebird');
+const connection = require('../mongo/connection');
 
 process.on('message', aggregateDB => {
-    try {
-        yield aggregate(aggregateDB);
-    } catch (e) {
-        console.error(e);
-        return process.exit(1);
-    }
-    process.exit(0);
+    Promise.coroutine(function* () {
+        try {
+            yield connection();
+            yield aggregate(aggregateDB);
+        } catch (e) {
+            console.error(e);
+            return process.exit(1);
+        }
+        process.exit(0);
+    })();
+    
 });
--- src/source/copyWorker.js
+++ src/source/copyWorker.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const source = require('./source');
+const Promise = require('bluebird');
+const connection = require('../mongo/connection');
 
 process.on('message', options => {
-    try {
-        yield source.copy(options);
-    } catch(e) {
-        console.error(e);
-        return process.exit(1);
-    }
+    Promise.coroutine(function* () {
+        try {
+            yield connection();
+            yield source.copy(options);
+        } catch(e) {
+            console.error(e);
+            return process.exit(1);
+        }
+        process.exit(0);
+    })();
 
-    process.exit(0);
 });
