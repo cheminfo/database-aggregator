4a176b5ac5254a96b27ab5fd9b707237abed9ca1
stropitek <kostro.d@gmail.com>
2016-04-25 10:36:26 +0200

    scheduler: generate process-scheduler configuration

---
+++ bin/scheduler.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const path = require('path');
+const Promise = require('bluebird');
+const debug = require('../src/util/debug')('bin:schedule');
+const connection = require('../src/mongo/connection');
+
+// Config
+const config = require('../src/config/config').globalConfig;
+const sources = Object.keys(config.source);
+const aggregations = Object.keys(config.aggregation);
+console.log(sources);
+console.log(aggregations);
+
+const syncHistory = require('../src/mongo/models/syncHistory');
+const schedulerLog = require('../src/mongo/models/schedulerLog');
+
+const processScheduler = require('process-scheduler');
+
+Promise.coroutine(function* () {
+    //yield connection();
+    const schedulerConfig = {
+        threads: 4
+    };
+
+    const schedule = [];
+    // Create configuration
+        for (const collection of sources) {
+            schedule.push({
+                id: 'source_copy_' + collection,
+                worker: path.join(__dirname, '../src/source/copyWorker.js'),
+                immediate: false,
+                cronRule: config.source[collection].copyCronRule,
+                deps: [],
+                arg: config.source[collection]
+            });
+            schedule.push({
+                id: 'source_remove_' + collection,
+                worker: path.join(__dirname, '../src/source/removeWorker.js'),
+                immediate: false,
+                cronRule: config.source[collection].removeCronRule,
+                deps: [],
+                noConcurrency: [],
+                arg: config.source[collection]
+            });
+        }
+
+        for (const collection of aggregations) {
+            let aggId = 'aggregation_' + collection;
+            schedule.push({
+                id: aggId,
+                worker: path.join(__dirname, '../src/aggregation/worker.js'),
+                immediate: false,
+                arg: collection
+            });
+
+            let sources = Object.keys(config.aggregation[collection].sources);
+            for(const source of sources) {
+                let s = schedule.find(s => s.id === 'source_copy_' + source);
+                if(s) {
+                    s.deps.push(aggId);
+                    s.noConcurrency.push(aggId);
+                }
+                s = schedule.find(s => s.id === 'source_remove_' + source);
+                if(s) {
+                    s.deps.push(aggId);
+                    s.noConcurrency.push(aggId);
+                }
+            }
+        }
+
+    console.log(schedule);
+
+})();
