044e4a65b234f7e3e57972375d8d2f228c9c5fae
David_Turbert <david.turbert@firmenich.com>
2016-05-17 15:56:50 +0200

    add complete example

---
+++ examples/homeDir/aggregation/test_001.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = {
+  disabled: false,
+  sources: {
+    test_001: function(data, result, pid, id) {
+      if (data.length > 1) {
+        throw new Error('Expecting only one element here');
+      }
+      if (data.length === 0) {
+        return result;
+      }
+      data = data[0];
+      if (!result.header) result.header = {};
+      if (data.value2) {
+        result.header.description = data.value2;
+      }
+      if (data.value1) {
+        result.header.name = data.value1;
+      }
+      return result;
+    }
+  }
+};
