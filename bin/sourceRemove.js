c0aff82fb8dfc944d05af3aca8c9e60c8410fd32
stropitek <kostro.d@gmail.com>
2016-06-14 17:21:18 +0200

    add sourceRemove

---
+++ bin/sourceRemove.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Promise = require('bluebird');
+const pid = require('../src/util/pid');
+const connection = require('../src/mongo/connection');
+const debug = require('../src/util/debug')('bin:source');
+
+pid.start();
+
+const source = require('../src/source/source');
+const config = require('../src/config/config').globalConfig;
+
+const sources = Object.keys(config.source);
+
+if (sources.length === 0) {
+    console.log('no source found in config');
+}
+
+Promise.coroutine(function* () {
+    yield connection();
+    for (const collection of sources) {
+        let start = new Date().getTime();
+        debug(`Begin remove source of ${collection}`);
+        const options = config.source[collection];
+        try{
+            yield source.remove(options);
+        }catch(e){
+            console.error(e);
+        }
+        let end = new Date().getTime();
+        let time = end - start;
+        debug(`End remove source of ${collection} in ${time}ms`);
+    }
+})().then(function () {
+    console.log('finished');
+    return 0;
+}, function (e) {
+    console.error(e);
+    return 1;
+}).then(pid.stop);
