54b052372ee766bdd13a7cd09e5df66dfab47318
Daniel Kostro <kostro.d@gmail.com>
2018-04-23 15:27:11 +0200

    mongoose mocker

--- test/sources.js
+++ test/sources.js
@@ -1,5 +1,15 @@
 'use strict';
+var Mongoose = require('mongoose').Mongoose;
+var Mockgoose = require('mockgoose').Mockgoose;
 
+var mongoose = new Mongoose();
+var mockgoose = new Mockgoose(mongoose);
+
+before(function () {
+  return mockgoose
+    .prepareStorage()
+    .then(() => console.log('prepare storage done'));
+});
 const sources = require('../src/mongo/models/source');
 
 const data = require('./data/data');
@@ -7,6 +17,9 @@ const data = require('./data/data');
 describe('Sources', function () {
   beforeEach(data);
   it('Should create aggregated collection', function () {
-    return sources.getCommonIds('names', 0).should.eventually.be.an.Array;
+    return sources.getCommonIds('names', 0).then((ids) => {
+      console.log(ids);
+      return ids.should.be.an.Array;
+    });
   });
 });
