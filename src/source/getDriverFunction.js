9a740d9d251340c0272fd71f87fb6f99089b1013
Michaël Zasso <targos@protonmail.com>
2018-04-24 11:21:09 +0200

    allow absolute path for driver

--- src/source/getDriverFunction.js
+++ src/source/getDriverFunction.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const path = require('path');
+
 function getDriverFunction(driver, name) {
+  if (!path.isAbsolute(driver)) {
+    driver = path.join('../driver', driver);
+  }
   try {
     // eslint-disable-next-line import/no-dynamic-require
-    return require(`../driver/${driver}/${name}`);
+    return require(path.join(driver, name));
   } catch (e) {
     throw new Error(`driver not found: ${driver}`);
   }
