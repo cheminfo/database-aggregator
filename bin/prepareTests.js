6b45ab9535168b67b921df80bcb7393712899a88
Michaël Zasso <targos@protonmail.com>
2018-04-24 11:16:08 +0200

    eslint fix

--- bin/prepareTests.js
+++ bin/prepareTests.js
@@ -5,9 +5,8 @@ const { prepare } = require('../test/helper.js');
 prepare()
   .then(() => {
     console.log('test env preparation done');
-    process.exit(0);
   })
   .catch((e) => {
     console.error(e.message);
-    process.exit(1);
+    process.exitCode = 1;
   });
