e0689bf6cd8e508e2507c75f622b1121aa1f0493
Michaël Zasso <targos@protonmail.com>
2018-08-28 11:26:47 +0200

    try to migrate "all" route
    Still need to setup the database connection.

--- api/app/Controllers/Http/SchedulerController.js
+++ api/app/Controllers/Http/SchedulerController.js
@@ -1,8 +1,17 @@
 'use strict';
 
+const Model = use('Src/mongo/model').getSchedulerLog();
+
 class SchedulerController {
-  all() {
-    return { all: true };
+  all({ request }) {
+    const query = request.get();
+    const since = +query.since || 0;
+    const limit = +query.limit || 50;
+    return Model.find({})
+      .skip(since)
+      .limit(limit)
+      .lean(true)
+      .exec();
   }
 
   trigger({ params }) {
