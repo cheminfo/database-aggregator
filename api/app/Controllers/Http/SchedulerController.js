94400748acda269a16cb26ba7f4806c1d23eff24
Michaël Zasso <targos@protonmail.com>
2018-08-29 14:01:13 +0200

    feat: migrate API server to Adonis (#43)
    * start working on adonis api server
    
    * add example .env
    
    * Start creating the scheduler controller
    
    * try to migrate "all" route
    
    Still need to setup the database connection.
    
    * use adonis server
    
    * cleanup
    
    * add mongoose provider and expose aggregator config
    
    * setup port from aggregator config
    
    * create server instance from config
    
    * remove .env
    
    * set host
    
    * remove comments
    
    * remove now unused dependencies
    
    * upgrade process-scheduler
    
    * upgrade process-scheduler

---
+++ api/app/Controllers/Http/SchedulerController.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Model = use('Src/mongo/model').getSchedulerLog();
+const { triggerTask } = use('Src/scheduler');
+
+class SchedulerController {
+  all({ request }) {
+    const query = request.get();
+    const since = +query.since || 0;
+    const limit = +query.limit || 50;
+    return Model.find({})
+      .skip(since)
+      .limit(limit)
+      .lean(true)
+      .exec();
+  }
+
+  trigger({ params }) {
+    triggerTask(params.taskId);
+    return { ok: true };
+  }
+
+  async tasks({ request }) {
+    const query = request.get();
+    const since = query.since || 0;
+    const limit = query.limit || 1;
+    const results = [];
+    const tasksList = await Model.find().distinct('taskId');
+    for (const taskId of tasksList) {
+      const history = await Model.find({ taskId })
+        .sort({
+          date: -1
+        })
+        .skip(since)
+        .limit(limit)
+        .lean(true)
+        .exec();
+      results.push({
+        taskId,
+        history
+      });
+    }
+    return results;
+  }
+}
+
+module.exports = SchedulerController;
--- bin/database-aggregator.js
+++ bin/database-aggregator.js
@@ -2,25 +2,10 @@
 'use strict';
 
 require('make-promises-safe');
-const app = require('../src/api/server');
-const config = require('../src/config/config').globalConfig;
-const debug = require('../src/util/debug')('bin:server');
 const { connect } = require('../src/mongo/connection');
 const { start } = require('../src/scheduler');
 
 connect(); // Connect to mongodb database
 start(); // Start the scheduler
 
-if (config.ssl) {
-  require('https')
-    .createServer(config.ssl, app.callback())
-    .listen(config.port, function () {
-      debug.warn(`running on https://localhost:${config.port}`);
-    });
-} else {
-  require('http')
-    .createServer(app.callback())
-    .listen(config.port, function () {
-      debug.warn(`running on http://localhost:${config.port}`);
-    });
-}
+require('../api/server');
