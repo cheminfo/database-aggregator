b5fc64568f3b06100415e04a86252866b9842478
Michaël Zasso <targos@protonmail.com>
2018-08-29 12:38:19 +0200

    remove comments

--- bin/database-aggregator.js
+++ bin/database-aggregator.js
@@ -2,8 +2,6 @@
 'use strict';
 
 require('make-promises-safe');
-// const config = require('../src/config/config').globalConfig;
-// const debug = require('../src/util/debug')('bin:server');
 const { connect } = require('../src/mongo/connection');
 const { start } = require('../src/scheduler');
 
@@ -11,18 +9,3 @@ connect(); // Connect to mongodb database
 start(); // Start the scheduler
 
 require('../api/server');
-
-/* if (config.ssl) {
-  require('https')
-    .createServer(config.ssl, app.callback())
-    .listen(config.port, function () {
-      debug.warn(`running on https://localhost:${config.port}`);
-    });
-} else {
-  require('http')
-    .createServer(app.callback())
-    .listen(config.port, function () {
-      debug.warn(`running on http://localhost:${config.port}`);
-    });
-}
-*/
