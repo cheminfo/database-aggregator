55445d337316db3d1775103792999d26951e5719
Daniel Kostro <kostro.d@gmail.com>
2018-09-05 17:45:47 +0200

    use mono font for Card tag

--- front/src/components/CardTag.js
+++ front/src/components/CardTag.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CardTag(props) {
   const { value } = props;
   return (
-    <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm">
+    <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-mono">
       {value}
     </span>
   );
