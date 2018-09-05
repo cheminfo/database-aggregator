68dba89f41ae3326faa299566848f4618900c1a3
Michaël Zasso <targos@protonmail.com>
2018-09-05 15:14:35 +0200

    api: add exception handler
    log errors to the console

---
+++ api/app/Exceptions/Handler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const BaseExceptionHandler = use('BaseExceptionHandler');
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle(...args) {
+    return super.handle(...args);
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report(error, { request }) {
+    console.log(error);
+  }
+}
+
+module.exports = ExceptionHandler;
