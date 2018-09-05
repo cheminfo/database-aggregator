9bcca433c747916f4e363a6516c3bb13970fe36b
Michaël Zasso <targos@protonmail.com>
2018-09-05 11:11:35 +0200

    fix(front): send credentials in dev

--- front/src/axios.js
+++ front/src/axios.js
@@ -4,5 +4,6 @@ export const axios = axiosLib.create({
   baseURL:
     process.env.NODE_ENV === 'production'
       ? '/api/'
-      : 'http://localhost:6768/api/'
+      : 'http://localhost:6768/api/',
+  withCredentials: process.env.NODE_ENV === 'production'
 });
