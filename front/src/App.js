6bd79176d3f5bb101b061179465b4a1adcd1cf90
Michaël Zasso <targos@protonmail.com>
2018-09-05 08:44:46 +0200

    use tailwind stylesheet

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
