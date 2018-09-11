d1fea408f64a750d5973f5109f4635830094c887
Michaël Zasso <targos@protonmail.com>
2018-09-11 13:27:40 +0200

    fix(scheduler): create a unique index on scheduler log and retry writes

--- src/mongo/models/schedulerLog.ts
+++ src/mongo/models/schedulerLog.ts
@@ -1,8 +1,9 @@
 import { ISchedulerLogEntry } from '../../types';
 import { getSchedulerLog } from '../model';
+
 const Model = getSchedulerLog();
 
-export function save(obj: ISchedulerLogEntry) {
+export async function save(obj: ISchedulerLogEntry) {
   const stat = {
     status: obj.status,
     date: new Date(),
@@ -11,14 +12,31 @@ export function save(obj: ISchedulerLogEntry) {
     stderr: obj.stderr
   };
 
-  return Model.findOneAndUpdate(
-    { pid: obj.pid },
-    {
-      $push: { state: stat },
-      taskId: obj.id,
-      date: new Date(),
-      pid: obj.pid
-    },
-    { upsert: true, new: true }
-  ).exec();
+  function update() {
+    return Model.findOneAndUpdate(
+      { pid: obj.pid },
+      {
+        $push: { state: stat },
+        taskId: obj.id,
+        date: new Date(),
+        pid: obj.pid
+      },
+      {
+        upsert: true,
+        new: true
+      }
+    ).exec();
+  }
+
+  while (true) {
+    try {
+      return await update();
+    } catch (e) {
+      // This error can happen if two upserts with the same pid are done very
+      // quickly
+      if (e.codeName !== 'DuplicateKey') {
+        throw e;
+      }
+    }
+  }
 }
