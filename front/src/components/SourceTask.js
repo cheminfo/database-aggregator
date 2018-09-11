522b5ab297311d6dae8ff1dd5cd3d0fea9d4ebdd
Michaël Zasso <targos@protonmail.com>
2018-09-11 10:32:40 +0200

    front: parse cron rule and display next executions of sources

--- front/src/components/SourceTask.js
+++ front/src/components/SourceTask.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
+import SourceTaskCron from './SourceTaskCron';
+
 export default function SourceTask(props) {
   const { task } = props;
+  const hasNextExecutions =
+    task.copyCronRule || task.copyMissingIdsCronRule || task.removeCronRule;
   return (
     <div>
       <div className="font-bold text-xl mb-2">{task.collection}</div>
       <div>
-        <div className="font-bold mb-2">Cron rules</div>
-        <ul>
-          <li>Copy: {task.copyCronRule}</li>
-          <li>Copy missing: {task.copyMissingIdsCronRule}</li>
-          <li>Remove: {task.removeCronRule}</li>
-        </ul>
+        <div className="font-bold mb-2">Next executions</div>
+        {hasNextExecutions ? (
+          <ul>
+            <SourceTaskCron label="Copy" value={task.copyCronRule} />
+            <SourceTaskCron
+              label="Copy missing"
+              value={task.copyMissingIdsCronRule}
+            />
+            <SourceTaskCron label="Remove" value={task.removeCronRule} />
+          </ul>
+        ) : (
+          'Nothing planned'
+        )}
       </div>
     </div>
   );
