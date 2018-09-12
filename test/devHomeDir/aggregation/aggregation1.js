9055975801a953fdaaceebb7913937c7a684ef8c
Michaël Zasso <targos@protonmail.com>
2018-09-12 13:46:22 +0200

    implement aggregation and do source1 every 5 minutes

--- test/devHomeDir/aggregation/aggregation1.js
+++ test/devHomeDir/aggregation/aggregation1.js
@@ -1,8 +1,12 @@
 'use strict';
 
-module.exports = {
-  cronRule: '0 0 * * *',
+/** @type {import('../../../src/types').IAggregationConfigFile} */
+const aggregation = {
   sources: {
-    source1: () => {}
+    source1: (data, result) => {
+      result.source1 = data[0];
+    }
   }
 };
+
+module.exports = aggregation;
--- test/devHomeDir/source/source1.js
+++ test/devHomeDir/source/source1.js
@@ -1,6 +1,9 @@
 'use strict';
 
-module.exports = {
-  copyCronRule: '* * * * *',
+/** @type {import('../../../src/types').ISourceConfigFile} */
+const source = {
+  copyCronRule: '*/5 * * * *',
   driver: require('../driver/constantUpdates')
 };
+
+module.exports = source;
