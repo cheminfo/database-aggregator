492af74c017acb2148f07c06714d8778a27cb6d7
Michaël Zasso <targos@protonmail.com>
2018-09-25 10:51:00 +0200

    always include tooltip in status svg

--- front/src/components/TaskCard.js
+++ front/src/components/TaskCard.js
@@ -2,20 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import StatusSvg from './svg/StatusSvg';
 
-const statusTextMap = {
-  null: 'This task has never been scheduled',
-  error: 'Last execution of this task failed',
-  queued: 'This task is currently queued',
-  success: 'Last execution of this task succeeded'
-};
-
 export default function TaskCard(props) {
   const { enabled, status, children } = props;
   const opacity = classNames({ 'opacity-25': !enabled });
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-md m-auto px-6 py-4 mb-6 relative">
       <div className={opacity}>
-        <div className="absolute pin-r pr-6" title={statusTextMap[status]}>
+        <div className="absolute pin-r pr-6">
           <StatusSvg status={status} />
         </div>
         {children}
--- front/src/components/svg/StatusSvg.js
+++ front/src/components/svg/StatusSvg.js
@@ -4,17 +4,27 @@ const statusColorMap = {
   null: 'text-grey-light',
   error: 'text-red-light',
   queued: 'text-orange-light',
-  success: 'text-green-light',
-  running: 'text-blue-light'
+  running: 'text-blue-light',
+  success: 'text-green-light'
+};
+
+const statusTextMap = {
+  null: 'This task has never been scheduled',
+  error: 'Last execution of this task failed',
+  queued: 'This task is currently queued',
+  running: 'This task is running',
+  success: 'Last execution of this task succeeded'
 };
 
 export default function({ status }) {
   return (
-    <svg
-      className={`fill-current ${statusColorMap[status]} w-6 h-6`}
-      viewBox="0 0 100 100"
-    >
-      <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="3" />
-    </svg>
+    <div title={statusTextMap[status]}>
+      <svg
+        className={`fill-current ${statusColorMap[status]} w-6 h-6`}
+        viewBox="0 0 100 100"
+      >
+        <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="3" />
+      </svg>
+    </div>
   );
 }
