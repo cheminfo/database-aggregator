9dfff423c521c84e47f3e6e066dda416bc3214a8
Daniel Kostro <kostro.d@gmail.com>
2018-10-01 16:43:03 +0200

    feat: sort sources in task list

--- front/src/components/TaskListWithData.js
+++ front/src/components/TaskListWithData.js
@@ -6,6 +6,9 @@ const TaskListWithData = () => {
   return (
     <Polling interval={10000} url="scheduler/tasks">
       {({ data, error }) => {
+        if (data) {
+          data.sources.sort(sortTasks);
+        }
         return <TaskList tasks={data} error={error} />;
       }}
     </Polling>
@@ -13,3 +16,31 @@ const TaskListWithData = () => {
 };
 
 export default TaskListWithData;
+
+const statusScore = {
+  running: 2,
+  queued: 1,
+  none: 0
+};
+
+function sortTasks(a, b) {
+  const stateA = a.state;
+  const stateB = b.state;
+  return compareState(stateA, stateB);
+}
+
+function compareStatus(a, b) {
+  const statusA = a ? a.status : 'none';
+  const statusB = b ? b.status : 'none';
+  return (statusScore[statusB] || 0) - (statusScore[statusA] || 0);
+}
+
+function compareState(stateA, stateB) {
+  if (compareStatus(stateA, stateB) === 0) {
+    const dateA = stateA ? stateA.date.getTime() : 0;
+    const dateB = stateB ? stateB.date.getTime() : 0;
+    return dateB - dateA;
+  } else {
+    return compareStatus(stateA, stateB);
+  }
+}
