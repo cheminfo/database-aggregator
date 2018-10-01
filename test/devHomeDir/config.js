d355c5568f2865e0433acb49250c4cf80cf1a231
Michaël Zasso <targos@protonmail.com>
2018-10-01 11:59:30 +0200

    prepare middleware to test errors

--- test/devHomeDir/config.js
+++ test/devHomeDir/config.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = {};
+const TEST_ERROR = false;
+
+module.exports = {
+  middleware: async (ctx, next) => {
+    if (!TEST_ERROR) {
+      return next();
+    }
+    ctx.response.status(401);
+    ctx.response.send('unauthorized');
+  }
+};
